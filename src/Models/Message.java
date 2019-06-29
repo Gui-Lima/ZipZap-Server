ac095cbd1afce65d6b060c1fadb8609abf1c070a
Magnon Souza <mprs@cin.ufpe.br>
2019-06-29 14:28:39 -0300

    updated Message class and created Type enum

--- src/Models/Message.java
+++ src/Models/Message.java
@@ -1,37 +1,34 @@
 package Models;
 
-enum Type {
-    CONECT,
-    MESSAGE,
-    ENDCONECT
-}
-
 public class Message {
 
-    private String text;
-    private String toIP;
-    private String fromIP;
     public Type type;
+    private String fromPort;
+    private String toPort;
+    private String text;
 
-    public Message(String text, String toIP, String fromIP, Type type) {
-        this.text = text;
-        this.toIP = toIP;
-        this.fromIP = fromIP;
+    public Message(String text, String toPort, String fromPort, Type type) {
         this.type = type;
+        this.fromPort = fromPort;
+        this.toPort = toPort;
+        this.text = text;
     }
 
-    public Message(String encryp) {
-        String[] arr = encryp.split("||");
+    public Message(String encrypt) {
+        String[] arr = encrypt.split("||");
 
         this.type = Type.valueOf(arr[0]);
-        this.fromIP = arr[1];
-        this.toIP = arr[2];
+        this.fromPort = arr[1];
+        this.toPort = arr[2];
         this.text = arr[3];
     }
 
     public String toString() {
         StringBuilder message = new StringBuilder();
-        message.append(this.fromIP).append("||").append(this.toIP).append("||").append(this.text);
+        message.append(this.type.toString()).append("||")
+                .append(this.fromPort).append("||")
+                .append(this.toPort).append("||")
+                .append(this.text);
         return message.toString();
     }
 
@@ -39,12 +36,12 @@ public class Message {
         this.text = str;
     }
 
-    public void setToIP(String ip) {
-        this.toIP = ip;
+    public void setToPort(int port) {
+        this.toPort = Integer.toString(port);
     }
 
-    public void setFromIP(String ip) {
-        this.fromIP = ip;
+    public void setFromPort(int port) {
+        this.fromPort = Integer.toString(port);
     }
 
     public void setType(Type type) {
@@ -55,12 +52,12 @@ public class Message {
         return this.text;
     }
 
-    public String getToIP() {
-        return this.toIP;
+    public int getToPort() {
+        return Integer.parseInt(this.toPort);
     }
 
-    public String getFromIP() {
-        return this.fromIP;
+    public int getFromPort() {
+        return Integer.parseInt(this.fromPort);
     }
 
     public Type getType() {
---
+++ src/Models/Type.java
@@ -0,0 +1,7 @@
+package Models;
+
+public enum Type {
+    CONNECT,
+    MESSAGE,
+    FINISH
+}
